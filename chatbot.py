c00a957e5fbfee123c8f55fd914ed43b3b35b9d4
DataGuti <atvidau@gmail.com>
2025-04-02 21:51:07 -0400

    Finished version of script.

--- chatbot.py
+++ chatbot.py
@@ -8,23 +8,55 @@ This object will handle the following:
 
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
-model_name = "facebook/blenderbot-400M-distill"
-model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+def initialize_model():
+    """
+    Function to fetch a model and tokenizer from HuggingFace"
+    Returns a dictionary with a "model" and a "tokenizer"
+    """
+    model_name = "facebook/blenderbot-400M-distill"
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-conversation_history = []
+    return {"model" : model, "tokenizer" : tokenizer}
 
-history_string = "\n".join(conversation_history)
+def handle_conversation(model_and_tokenizer):
+    """
+    Loop that handles conversation and stores a conversation history.
+    Input is a dictionary with a model and tokenizer.
+    Look function initialize_model for more details on input
+    """
+    conversation_history = []
+    model = model_and_tokenizer["model"]
+    tokenizer = model_and_tokenizer["tokenizer"]
 
-input_text = "hello, how are you doing?"
+    # Chat loop. Open until closed from terminal.
+    while True:
+        
+        #Parse conversation history list into a string
+        history_string = "\n".join(conversation_history)
 
-inputs = tokenizer.encode_plus(history_string, input_text, return_tensors="pt")
+        input_text = input("> ")
 
-outputs = model.generate(**inputs,max_new_tokens=1000)
+        #Tokenize the input text AND the history for context.
+        inputs = tokenizer.encode_plus(history_string, input_text, return_tensors="pt")
 
-response = tokenizer.decode(outputs[0],skip_special_tokens=True).strip()
+        #Generate response from the model
+        outputs = model.generate(**inputs,max_new_tokens=1000)
 
-print(response)
+        #Decode the response into readable text
+        response = tokenizer.decode(outputs[0],skip_special_tokens=True).strip()
 
-conversation_history.append(input_text)
-conversation_history.append(response)
+        print(response)
+
+        #Update conversation history
+        conversation_history.append(input_text)
+        conversation_history.append(response)
+
+def main():
+    model_and_tokenizer = initialize_model()
+    handle_conversation(model_and_tokenizer)
+
+if __name__ == "__main__":
+    main()
+
+

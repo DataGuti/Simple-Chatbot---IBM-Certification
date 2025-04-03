a90eb25e6bd37803e529769d27aa45e8004a51c0
DataGuti <atvidau@gmail.com>
2025-04-02 21:31:00 -0400

    Finished console chatbot, basic undocumented version

--- chatbot.py
+++ chatbot.py
@@ -12,3 +12,19 @@ model_name = "facebook/blenderbot-400M-distill"
 model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
+conversation_history = []
+
+history_string = "\n".join(conversation_history)
+
+input_text = "hello, how are you doing?"
+
+inputs = tokenizer.encode_plus(history_string, input_text, return_tensors="pt")
+
+outputs = model.generate(**inputs,max_new_tokens=1000)
+
+response = tokenizer.decode(outputs[0],skip_special_tokens=True).strip()
+
+print(response)
+
+conversation_history.append(input_text)
+conversation_history.append(response)
